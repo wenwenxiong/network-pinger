0d730d70104d3772547837445e2a76f206449150
xiongwenwen01@163.com <xiongwenwen01@163.com>
2024-03-15 17:48:30 +0800

    remove k8s lable

--- pkg/pinger/ping.go
+++ pkg/pinger/ping.go
@@ -7,7 +7,6 @@ import (
 	"github.com/wenwenxiong/network-pinger/pkg/util"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/klog/v2"
 	"math"
 	"net"
