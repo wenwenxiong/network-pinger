42951def148462782a57990395454549fca070c0
xiongww <xiongww@chinatelecom.cn>
2024-03-15 17:41:25 +0800

    add ContainsString

---
+++ pkg/util/slice.go
@@ -0,0 +1,11 @@
+package util
+
+// ContainsString Helper functions to check and remove string from a slice of strings.
+func ContainsString(slice []string, s string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+	}
+	return false
+}
